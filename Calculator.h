085d052686b3774ba5118033da206c14878fcc6a
markojoksimovic9 <85893948+markojoksimovic9@users.noreply.github.com>
2021-06-18 16:37:47 -0500

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_FUNCTIONS_H
+
+#define CALCULATOR_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int add(int x, int y);
+int subtract(int x, int y);
+int multiply(int x, int y);
+int divide(int x, int y);
+
+#endif
