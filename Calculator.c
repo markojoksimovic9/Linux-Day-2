11d0ea91ddebeb0edbeb9e84095c3f9ba4e11366
markojoksimovic9 <85893948+markojoksimovic9@users.noreply.github.com>
2021-06-18 15:27:39 -0500

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,25 @@
+
+int add(int x, int y) {
+  
+  return x+y;
+}
+
+int subtract(int x, int y) {
+  
+  return x-y;
+}
+
+int multiply(int x, int y) {
+  
+  return x*y;
+}
+
+int divide(int x, int y) {
+  
+  return x/y;
+}
+
+int main() {
+  
+  return 1;
+}
